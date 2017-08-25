8381398f8840bbd6f1f2215d4e050832590b6f24
Satasuk vipaksinlapin <satasuk@gmail.com>
2017-08-25 12:09:22 +0700

    Display Greeting message when program is executed

---
+++ src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Git");
+		System.out.println("I am Satasuk Vipaksinlapin");
+	}
+
+}
